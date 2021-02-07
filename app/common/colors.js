af0040281ef3607d4ef17cd50ebb914047ac380a
Mahmoud Ali Ibrahim <ma7moud.saraya@gmail.com>
2021-02-07 22:18:42 +0200

    integrate camera module

--- app/common/colors.js
+++ app/common/colors.js
@@ -1,5 +1,6 @@
 const colors = {
   main_color: 'rgba(131, 189, 69, 1)',
+  background_color: 'rgba(255, 255, 255, 1)',
 };
 
 export default colors;
--- app/scenes/ScannerScene.js
+++ app/scenes/ScannerScene.js
@@ -1,14 +1,33 @@
 import React, {Component} from 'react';
-import {Text, StyleSheet, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import {RNCamera} from 'react-native-camera';
+import {colors} from '../common';
 
 export default class ScannerScene extends Component {
+  onBarCodeRead = (event) => {
+    console.log(event.data);
+  };
   render() {
     return (
-      <View>
-        <Text> Scanner Scene </Text>
+      <View style={styles.container}>
+        <RNCamera
+          style={styles.camera}
+          aspect={RNCamera.Constants.AutoFocus}
+          ref={(cam) => (this.camera = cam)}
+          onBarCodeRead={this.onBarCodeRead}
+          captureAudio={false}
+        />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.background_color,
+  },
+  camera: {
+    flex: 1,
+  },
+});
